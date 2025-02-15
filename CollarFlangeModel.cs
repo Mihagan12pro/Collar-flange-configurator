ebe50ed2621155c1cafe21f13ec77543e0621b76
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-02-15 21:42:48 +0300

    A Command class was created.

--- CollarFlangeModel.cs
+++
@@ -1,9 +0,0 @@
-using Mapinet;
-namespace Collar_flange_configurator
-{
-    public class CollarFlangeModel : ICollarFlange
-    {
-        
-    }
-
-}
---
+++ Command.cs
@@ -0,0 +1,18 @@
+﻿using Multicad.AplicationServices;
+using Multicad.CustomObjectBase;
+using Multicad.DatabaseServices.StandardObjects;
+using Multicad.Geometry;
+using Multicad.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Multicad.Constants.ObjectOverlays;
+namespace Collar_flange_configurator
+{
+    internal class Command
+    {
+       
+    }
+}
