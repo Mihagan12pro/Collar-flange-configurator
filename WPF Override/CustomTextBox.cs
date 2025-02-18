7bcab8c8bc5f0ce9619cfad9f495d1df908edddc
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-02-18 22:17:41 +0300

    The CustomTextBox class was edited (Added KeyDown and LostFocus event handlers, an IsValideKeyDown method was created). A NumberTextBox class is a child class of the CustomTextBox class.

--- WPF Override/CustomTextBox.cs
+++ WPF Override/CustomTextBox.cs
@@ -4,19 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Collar_flange_configurator.WPF_Override
 {
     internal abstract class CustomTextBox : TextBox
     {
+
         public CustomTextBox()
         {
             KeyDown += CustomTextBox_KeyDown;
+            LostFocus += CustomTextBox_LostFocus;
         }
+
+        private void CustomTextBox_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (Text.Length == 0)
+            {
+                Text += "0";
+            }
+        }
+
         private void CustomTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            NumberValidation(sender,e);
+           if (!IsValideKeyDown(e.Key))
+           {
+                e.Handled = true;
+           }
         }
-        protected abstract void NumberValidation(object sender, System.Windows.Input.KeyEventArgs e);
+
+        protected virtual bool IsValideKeyDown(System.Windows.Input.Key key)
+        {
+            if (key < Key.D0 || key > Key.D9)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
---
+++ WPF Override/NumberTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Collar_flange_configurator.WPF_Override
+{
+    internal class NumberTextBox : CustomTextBox
+    {
+        public NumberTextBox()
+        {
+            
+        }
+    }
+}
