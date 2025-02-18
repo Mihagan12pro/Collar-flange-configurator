b233250002240e2a463db24f4c567abae0f9895f
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-02-18 10:29:15 +0300

    A "WPF Override" folder has been created.

---
+++ WPF Override/CustomTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Collar_flange_configurator.WPF_Override
+{
+    internal abstract class CustomTextBox : TextBox
+    {
+        public CustomTextBox()
+        {
+            KeyDown += CustomTextBox_KeyDown;
+        }
+        private void CustomTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            NumberValidation(sender,e);
+        }
+        protected abstract void NumberValidation(object sender, System.Windows.Input.KeyEventArgs e);
+    }
+}
