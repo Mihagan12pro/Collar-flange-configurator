040b8f240a66d4087ca540b349a7e8ae6bf153b5
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-02-21 17:23:47 +0300

    The MainDialog class has been reduced to the project. A CollarFlangeMaster class has been removed from the project.

--- Command.cs
+++ Command.cs
@@ -14,26 +14,12 @@ namespace Collar_flange_configurator
 {
     internal class Command
     {
-        PaletteSet paletteSet;
-
-        [CommandMethod("Collar_flange")]
+        [CommandMethod("Collar_flange_master")]
         public void CreateMainDialog()
         {
-            
+            MainDialog main = new MainDialog();
 
-
-            paletteSet = new PaletteSet("Мастер воротникового фланца");
-            paletteSet.Size = new System.Drawing.Size(600, 600);
-          
-            paletteSet.MinimumSize = new System.Drawing.Size(300, 300);
-
-            CollarFlangeMaster master = new CollarFlangeMaster();
-
-            paletteSet.AddVisual("", master);
-
-            paletteSet.Visible = true;
-
-            
+            HostMgd.ApplicationServices.Application.ShowModelessWindow(main);
         }
     }
 }
