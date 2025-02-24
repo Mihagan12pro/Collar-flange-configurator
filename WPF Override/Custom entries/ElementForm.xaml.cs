58c6139f45f542e944229a96d59b9d85c77d818f
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-02-24 18:48:19 +0300

    A ElementForm user control has been created. A IntegerForm has been created.

---
+++ WPF Override/Custom entries/ElementForm.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Collar_flange_configurator.WPF_Override.Custom_entries
+{
+    /// <summary>
+    /// Логика взаимодействия для IntegerForm.xaml
+    /// </summary>
+    public abstract partial class ElementForm: UserControl
+    {
+
+        public string TextBoxText;
+        public string LabelText
+        {
+            get
+            {
+                return (string)ElementLabel.Content;
+            }
+            set
+            {
+                ElementLabel.Content = value;
+            }
+        }
+
+        public ElementForm()
+        {
+            InitializeComponent();  
+        }
+    }
+}
---
+++ WPF Override/Custom entries/IntegerForm.cs
@@ -0,0 +1,25 @@
+﻿using Collar_flange_configurator.WPF_Override.TextBox;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collar_flange_configurator.WPF_Override.Custom_entries
+{
+    public class IntegerForm : ElementForm
+    {
+        IntegerTextBox textBox;
+        public IntegerForm()
+        {
+            textBox = new IntegerTextBox();
+            textBox.FontFamily = new System.Windows.Media.FontFamily("GOST Type B");
+            textBox.Width = 100;
+            textBox.Height = 15;
+
+            ElementStackPanel.Children.Add(textBox);
+        }
+    }
+}
