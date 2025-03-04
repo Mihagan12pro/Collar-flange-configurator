e7a0ff88332a83408e0944a6abf2fc256c34c048
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-03-04 17:08:26 +0300

    A "Validation classes" folder has been cretaed.

--- ViewModel/CollarFlangeViewModel.cs
+++ ViewModel/CollarFlangeViewModel.cs
@@ -14,6 +14,10 @@ namespace Collar_flange_configurator.ViewModel
     internal class CollarFlangeViewModel : INotifyPropertyChanged
     {
         private bool isConfig3DModel;
+
+        private bool isSizeDmValid, isSizeDnValid, isSized1Valid, isSizebValid, isSizeHValid, isSizeH1Valid, isSizeDValid;
+        private bool isSizeD1Valid, isSizedValid, isSizeR1Valid, isSizel1Valid, isSizefValid,isCountnValid;
+
         public bool IsConfig3DModel
         {
             get
@@ -81,215 +85,10 @@ namespace Collar_flange_configurator.ViewModel
 
 
 
-        private bool isSizeDmValid;
-        public bool IsSizeDmValid
-        {
-            get
-            {
-                return isSizeDmValid;
-            }
-            set
-            {
-                isSizeDmValid = value;
-                OnPropertyChanged();
-            }
-        }
 
-
-        private bool isSizeDnValid;
-        public bool IsSizeDnValid
-        {
-            get
-            {
-                return isSizeDnValid;
-            }
-            set
-            {
-                isSizeDnValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSized1Valid;
-        public bool IsSized1Valid
-        {
-            get
-            {
-                return isSized1Valid;
-            }
-            set
-            {
-                isSized1Valid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizebValid;
-        public bool IsSizebValid
-        {
-            get
-            {
-                return isSizebValid;
-            }
-            set
-            {
-                isSizebValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizeHValid;
-        public bool IsSizeHValid
-        {
-            get
-            {
-                return isSizeHValid;
-            }
-            set
-            {
-                isSizeHValid = value;
-                OnPropertyChanged( );
-            }
-        }
-
-
-        private bool isSizeH1Valid;
-        public bool IsSizeH1Valid
-        {
-            get
-            {
-                return isSizeH1Valid;
-            }
-            set
-            {
-                isSizeH1Valid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizeDValid;
-        public bool IsSizeDValid
-        {
-            get
-            {
-                return isSizeDValid;
-            }
-            set
-            {
-                isSizeDValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizeD1Valid;
-        public bool IsSizeD1Valid
-        {
-            get
-            {
-                return isSizeD1Valid;
-            }
-            set
-            {
-                isSizeD1Valid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizedValid;
-        public bool IsSizedValid
-        {
-            get
-            {
-                return isSizedValid;
-            }
-            set
-            {
-                isSizedValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizeR1Valid;
-        public bool IsSizeR1Valid
-        {
-            get
-            {
-                return isSizeR1Valid;
-            }
-            set
-            {
-                isSizeR1Valid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizel1Valid;
-        public bool IsSizel1Valid
-        {
-            get
-            {
-                return isSizel1Valid;
-            }
-            set
-            {
-                isSizel1Valid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isSizefValid;
-        public bool IsSizefValid
-        {
-            get
-            {
-                return isSizefValid;
-            }
-            set
-            {
-                isSizefValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private bool isCountnValid;
-        public bool IsCountnValid
-        {
-            get
-            {
-                return isCountnValid;
-            }
-            set
-            {
-                isCountnValid = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-
-        private double valueDmSize;
-        public double ValueDmSize
-        {
-            get
-            {
-                return valueDmSize;
-            }
-            set
-            {
-                valueDmSize = value;
-                OnPropertyChanged();
-            }
-        }
+       
+ 
+        
 
 
 
@@ -305,8 +104,6 @@ namespace Collar_flange_configurator.ViewModel
             IsConfig3DModel = true;
 
             IsOkEnabled = true;
-
-            IsSizeDmValid = true;
         }
     }
 }
