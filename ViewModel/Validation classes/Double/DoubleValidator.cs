c2bff9b5a1298b0554b9aef327ebaa4b9f14e2fc
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-03-07 11:27:31 +0300

    The DoubleValidator class got a children classes,

--- ViewModel/Validation classes/Double/DoubleValidator.cs
+++ ViewModel/Validation classes/Double/DoubleValidator.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Collar_flange_configurator.ViewModel.Validation_classes
 {
-    class DoubleValidator : ParamValidator
+    public class DoubleValidator : ParamValidator
     {
+        protected List<DoubleValidator> dominantList,oppressedList;
         public override bool CheckValidation(object param)
         {
             if (!int.TryParse(Convert.ToString(param), out int i))
