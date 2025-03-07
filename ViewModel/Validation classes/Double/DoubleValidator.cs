ab5349a7edddf2504d7ecd3fa211825119f94b37
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-03-07 11:42:41 +0300

    The DoubleValidator class lost its children. The DoubleValidator class was updated.

--- ViewModel/Validation classes/Double/DoubleValidator.cs
+++ ViewModel/Validation classes/Double/DoubleValidator.cs
@@ -8,13 +8,34 @@ namespace Collar_flange_configurator.ViewModel.Validation_classes
 {
     public class DoubleValidator : ParamValidator
     {
-        protected List<DoubleValidator> dominantList,oppressedList;
+        public List<DoubleValidator> DominantsList = new List<DoubleValidator>();
+        public List<DoubleValidator> OppressedList = new List<DoubleValidator>();
+
+        public double Value{get; private set;}
         public override bool CheckValidation(object param)
         {
-            if (!int.TryParse(Convert.ToString(param), out int i))
+            if (!double.TryParse(Convert.ToString(param), out double d))
             {
                 return false;
             }
+            Value = d;
+
+            foreach(DoubleValidator dominant in DominantsList)
+            {
+                if (dominant.Value <= Value)
+                {
+                    return false;
+                }
+            }
+
+            foreach(DoubleValidator oppressed in OppressedList)
+            {
+                if (oppressed.Value >= Value)
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
     }
